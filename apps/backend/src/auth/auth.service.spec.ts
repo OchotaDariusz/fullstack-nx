d27e981d6b2b7fc65e4859cb05bfc1d3ffd38501
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-04-02 13:13:17 +0200

    tests: fix for tests

--- apps/backend/src/auth/auth.service.spec.ts
+++ apps/backend/src/auth/auth.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../users/entity/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        JwtService,
+        UsersService,
+        {
+          provide: Repository<UserEntity>,
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            find: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
