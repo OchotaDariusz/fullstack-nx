680b122b7112ebd23024d8e52259e8aa85e26f1e
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-04-06 12:54:45 +0200

    refactor: use axios

--- libs/data-manager/src/lib/data-manager.tsx
+++ libs/data-manager/src/lib/data-manager.tsx
@@ -1,13 +1,13 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:4200",
+  baseURL: 'http://localhost:4200',
   timeout: 300000, // 5 minutes
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
-    'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+    Authorization: `Bearer ${localStorage.getItem('access_token')}`,
   },
 });
 
