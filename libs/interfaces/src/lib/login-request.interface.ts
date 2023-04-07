f55a24a29e133bcf734b70897463ff06505a8a06
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-04-07 20:45:52 +0200

    feature: redux holds auth state

--- libs/interfaces/src/lib/login-request.interface.ts
+++ libs/interfaces/src/lib/login-request.interface.ts
@@ -3,11 +3,3 @@ export interface LoginRequest {
   username?: string;
   password: string;
 }
-
-interface MathOperations {
-  add(num: number): void;
-  subtract(num: number): void;
-  divide(num: number): void;
-  multiply(num: number): void;
-  getResult(): number;
-}
