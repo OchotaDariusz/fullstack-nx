4fa8f28728f3c7fbcccc97e278c6a38588b1c565
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-06-18 00:39:57 +0200

    fix(tests): health controller unit tests

--- apps/backend/src/health/health.controller.spec.ts
+++ apps/backend/src/health/health.controller.spec.ts
@@ -1,11 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { HealthController } from './health.controller';
+import { HttpModule } from '@nestjs/axios';
+import { TerminusModule } from '@nestjs/terminus';
+import { TerminusLoggerService } from './terminus-logger.service';
 
 describe('HealthController', () => {
   let controller: HealthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule,
+        TerminusModule.forRoot({
+          logger: TerminusLoggerService,
+          errorLogStyle: 'pretty',
+        }),
+      ],
       controllers: [HealthController],
     }).compile();
 
