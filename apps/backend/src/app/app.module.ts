1f92a5102170ecba92d939d0c241075f0e0eece5
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-06-24 09:06:07 +0200

    refactor: database seeder

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AuthModule } from '../auth/auth.module';
 import entities from '../entities';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { UsersModule } from '../users/users.module';
-import { SeedModule } from "../seed/seed.module";
+import { SeedModule } from '../seed/seed.module';
 
 @Module({
   imports: [
@@ -32,7 +32,7 @@ import { SeedModule } from "../seed/seed.module";
     }),
     AuthModule,
     UsersModule,
-    SeedModule
+    SeedModule,
   ],
   controllers: [AppController],
   providers: [
